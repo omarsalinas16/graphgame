d5b4646212668fc8bbf714d629c81cfee0a975b0
CASA\Ramon <eduardodelgado369@gmail.com>
2017-07-03 16:07:54 -0600

    Agujeros sin completar

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Parabox.CSG;
 
 [RequireComponent(typeof(PlayerController))]
 [RequireComponent(typeof(FormBehaviour))]
@@ -15,6 +16,13 @@ public class GameController : MonoBehaviour {
 	[SerializeField]
 	private int maxTransformAttempts = 5;
 	private int _transformAttempts;
+	
+	public GameObject xPlane;
+	public GameObject zPlane;
+	private Vector3 newPosition;
+	private Vector3 newScale;
+	private Vector3 newRotation;
+	
 	public int transformAttempts {
 		get {
 			return _transformAttempts;
@@ -47,11 +55,85 @@ public class GameController : MonoBehaviour {
 
 	private void Start() {
 		spawnForm();
+		putRandomTransform();
+		makeWholeHole(xPlane);
+		makeHole(zPlane,Vector3.zero);
 	}
-
+	
 	private void Update() {
-
+		if ( Input.GetKey(KeyCode.DownArrow) ) {
+			//putRandomTransform();
+			makeWholeHole(xPlane);
+			//makeHole(zPlane);
+		}
 	}
+	
+	private void putRandomTransform() {
+		//reposition();
+		rescale();
+		rotateRandom();
+	}
+	
+	//beggin
+	//Added by eduardo
+	
+	private void makeWholeHole(GameObject objPlane) {
+		makeHole(objPlane, Vector3.zero);
+		float mov = 2.0f;
+		while( mov < -2.0f ) { 
+			makeHole(objPlane, new Vector3(0.0f,0.0f,mov));
+			mov -= 0.3f;
+		}
+	}
+	
+	private void makeHole(GameObject objPlane,Vector3 positionMov) {
+		GameObject form = activeForm.gameObject;
+		Vector3 originalPos = objPlane.transform.position;
+		objPlane.transform.position = positionMov;
+		
+		Mesh m = CSG.Subtract(objPlane, form);
+		
+		objPlane.GetComponent<MeshFilter>().sharedMesh = m;
+		
+		objPlane.transform.position = originalPos;
+		objPlane.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+	
+	private void reposition() {
+		GameObject form = activeForm.gameObject;
+		newPosition = new Vector3(
+								putRandomPosition(), 
+								putRandomPosition(), 
+								putRandomPosition());
+		form.transform.position = newPosition;
+	}
+	
+	private void rescale() {
+		GameObject form = activeForm.gameObject;
+		newScale = new Vector3(
+								putRandomPosition(), 
+								putRandomPosition(), 
+								putRandomPosition());
+		form.transform.localScale = newScale;
+	}
+	
+	private void rotateRandom() {
+		GameObject form = activeForm.gameObject;
+		form.transform.RotateAround(Vector3.up,putRandomPosition());
+		form.transform.RotateAround(Vector3.right,putRandomPosition());
+		form.transform.RotateAround(Vector3.forward,putRandomPosition());
+	}
+	
+	private float putRandomPosition() {
+		float randomN = Mathf.Round(Random.Range(-2.0f, 2.0f));
+		if(randomN == 0)
+			randomN = 0.5f;
+		randomN = Mathf.Abs(randomN);
+		return randomN;
+	}
+	//end
+	
+	
 
 	public void initTransformAttempts() {
 		transformAttempts = maxTransformAttempts;
@@ -62,7 +144,7 @@ public class GameController : MonoBehaviour {
 			Form selectedForm = forms[shapeIndex];
 
 			activeForm = Instantiate(selectedForm.shape, formSpawn.position, Quaternion.identity, formSpawn);
-
+			
 			if (activeForm) {
 				if (FormBehaviour.Instance) {
 					FormBehaviour.Instance.fadeIn(activeForm);
@@ -76,6 +158,8 @@ public class GameController : MonoBehaviour {
 			initTransformAttempts();
 		}
 	}
+	
+	
 
 	private void endCurrentLevel() {
 		Debug.Log("WIN");
