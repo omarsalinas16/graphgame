db55e500039ef435049aabff57b3387bd8d7bc3b
DESKTOP-1VS1P6K\Omar Salinas <omar.salinas1611@gmail.com>
2017-07-03 20:46:26 -0600

    -Minor changes to the plane hole script.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -17,12 +17,6 @@ public class GameController : MonoBehaviour {
 	private int maxTransformAttempts = 5;
 	private int _transformAttempts;
 	
-	public GameObject xPlane;
-	public GameObject zPlane;
-	private Vector3 newPosition;
-	private Vector3 newScale;
-	private Vector3 newRotation;
-	
 	public int transformAttempts {
 		get {
 			return _transformAttempts;
@@ -36,6 +30,19 @@ public class GameController : MonoBehaviour {
 			}
 		}
 	}
+
+	[Header("Planes")]
+	[SerializeField]
+	private Transform xPlane;
+	[SerializeField]
+	private Transform zPlane;
+
+	[SerializeField]
+	private float axisPositionMin = -3.0f;
+	[SerializeField]
+	private float axisPositionMax = 3.0f;
+	[SerializeField]
+	private float axisStep = 0.3f;
 
 	[Header("Shapes")]
 	[SerializeField]
@@ -55,50 +62,60 @@ public class GameController : MonoBehaviour {
 
 	private void Start() {
 		spawnForm();
-		putRandomTransform();
-		makeWholeHole(xPlane);
-		makeHole(zPlane,Vector3.zero);
+		makeBothHoles();
+		fadeInForm();
 	}
 	
+	/*
 	private void Update() {
 		if ( Input.GetKey(KeyCode.DownArrow) ) {
-			//putRandomTransform();
+			putRandomTransform();
 			makeWholeHole(xPlane);
-			//makeHole(zPlane);
+			makeHole(zPlane);
 		}
 	}
 	
 	private void putRandomTransform() {
-		//reposition();
+		reposition();
 		rescale();
 		rotateRandom();
 	}
+	*/
 	
 	//beggin
 	//Added by eduardo
+
+	private void makeBothHoles() {
+		makeHole(xPlane, Vector3.right);
+		makeHole(zPlane, Vector3.forward);
+	}
 	
-	private void makeWholeHole(GameObject objPlane) {
-		makeHole(objPlane, Vector3.zero);
-		float mov = 2.0f;
-		while( mov < -2.0f ) { 
-			makeHole(objPlane, new Vector3(0.0f,0.0f,mov));
-			mov -= 0.3f;
+	private void makeHole(Transform plane, Vector3 moveAxis) {
+		Vector3 originalPos = plane.position;
+		float axisPosition = axisPositionMax;
+
+		Mesh mesh = null;
+
+		while (axisPosition > axisPositionMin) {
+			plane.position = moveAxis * axisPosition;
+			mesh = CSG.Subtract(plane.gameObject, activeForm.gameObject);
+
+			MeshFilter meshFilter = plane.GetComponent<MeshFilter>();
+
+			if (meshFilter != null && mesh != null) {
+				meshFilter.sharedMesh = mesh;
+
+				//change mesh collider too.
+			}
+
+			plane.position = originalPos;
+			plane.localScale = Vector3.one;
+
+			axisPosition -= axisStep;
 		}
 	}
 	
-	private void makeHole(GameObject objPlane,Vector3 positionMov) {
-		GameObject form = activeForm.gameObject;
-		Vector3 originalPos = objPlane.transform.position;
-		objPlane.transform.position = positionMov;
-		
-		Mesh m = CSG.Subtract(objPlane, form);
-		
-		objPlane.GetComponent<MeshFilter>().sharedMesh = m;
-		
-		objPlane.transform.position = originalPos;
-		objPlane.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-	}
-	
+	/*
 	private void reposition() {
 		GameObject form = activeForm.gameObject;
 		newPosition = new Vector3(
@@ -118,22 +135,22 @@ public class GameController : MonoBehaviour {
 	}
 	
 	private void rotateRandom() {
-		GameObject form = activeForm.gameObject;
-		form.transform.RotateAround(Vector3.up,putRandomPosition());
-		form.transform.RotateAround(Vector3.right,putRandomPosition());
-		form.transform.RotateAround(Vector3.forward,putRandomPosition());
+		activeForm.Rotate(Vector3.up, putRandomPosition());
+		activeForm.Rotate(Vector3.right, putRandomPosition());
+		activeForm.Rotate(Vector3.forward, putRandomPosition());
 	}
 	
 	private float putRandomPosition() {
 		float randomN = Mathf.Round(Random.Range(-2.0f, 2.0f));
-		if(randomN == 0)
+
+		if (randomN == 0) {
 			randomN = 0.5f;
-		randomN = Mathf.Abs(randomN);
-		return randomN;
+		}
+		
+		return Mathf.Abs(randomN);
 	}
+	*/
 	//end
-	
-	
 
 	public void initTransformAttempts() {
 		transformAttempts = maxTransformAttempts;
@@ -144,12 +161,6 @@ public class GameController : MonoBehaviour {
 			Form selectedForm = forms[shapeIndex];
 
 			activeForm = Instantiate(selectedForm.shape, formSpawn.position, Quaternion.identity, formSpawn);
-			
-			if (activeForm) {
-				if (FormBehaviour.Instance) {
-					FormBehaviour.Instance.fadeIn(activeForm);
-				}
-			}
 
 			if (PlayerController.Instance) {
 				PlayerController.Instance.setActiveForm(getActiveForm());
@@ -158,9 +169,15 @@ public class GameController : MonoBehaviour {
 			initTransformAttempts();
 		}
 	}
-	
-	
 
+	private void fadeInForm() {
+		if (activeForm) {
+			if (FormBehaviour.Instance) {
+				FormBehaviour.Instance.fadeIn(activeForm);
+			}
+		}
+	}
+	
 	private void endCurrentLevel() {
 		Debug.Log("WIN");
 
